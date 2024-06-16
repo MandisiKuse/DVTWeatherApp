183078d94b36072e4ec2f06b47625a6ef8a11114
mandisikuse <mandisikuse@gmail.com>
2024-06-16 23:49:02 +0200

    weather by city endpoint

--- app/src/main/java/com/dvt/weatherapp/data/remote/WeatherApi.kt
+++ app/src/main/java/com/dvt/weatherapp/data/remote/WeatherApi.kt
@@ -1,21 +1,21 @@
 package com.dvt.weatherapp.data.remote
 
-import com.dvt.weatherapp.data.model.CurrentWeather
-import com.dvt.weatherapp.data.model.Forecast
+import com.dvt.weatherapp.data.remote.model.CurrentWeather
+import com.dvt.weatherapp.data.remote.model.Forecast
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApi {
+
     companion object {
-        const val API_KEY = "140be93e468be72e0b012613a2456da1"
+        const val API_KEY = ""
         const val UNIT = "metric"
     }
 
     @GET("data/2.5/weather")
-    suspend fun getCurrentWeather(
-        @Query("lat") lat: Double,
-        @Query("lon") lon: Double,
+    suspend fun getWeatherByCity(
+        @Query("q") city: String,
         @Query("appid") apiKey: String = API_KEY,
         @Query("units") units: String = UNIT
     ): Response<CurrentWeather>

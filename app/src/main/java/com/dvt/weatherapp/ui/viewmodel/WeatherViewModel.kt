0303bd908e853c97c3166d5d74698391f20cd468
mandisikuse <mandisikuse@gmail.com>
2024-06-16 23:56:55 +0200

    Updating viewModel

--- app/src/main/java/com/dvt/weatherapp/ui/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/dvt/weatherapp/ui/viewmodel/WeatherViewModel.kt
@@ -2,11 +2,12 @@ package com.dvt.weatherapp.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.dvt.weatherapp.domain.mapper.CityWeatherMapper
 import com.dvt.weatherapp.domain.mapper.WeatherMapper
 import com.dvt.weatherapp.domain.repository.CurrentLocationRepository
 import com.dvt.weatherapp.domain.repository.WeatherRepository
 import com.dvt.weatherapp.ui.statemodel.CurrentWeatherState
-import com.dvt.weatherapp.ui.statemodel.ForecastState
+import com.dvt.weatherapp.ui.statemodel.WeatherByCityState
 import com.dvt.weatherapp.util.Resource
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -16,54 +17,53 @@ import kotlinx.coroutines.launch
 class WeatherViewModel(
     private val weatherRepository: WeatherRepository,
     private val locationRepository: CurrentLocationRepository,
-    private val weatherMapper: WeatherMapper
+    private val weatherMapper: WeatherMapper,
+    private val cityWeatherMapper: CityWeatherMapper
 ) : ViewModel() {
 
     //Properties Section
-    private val _weatherStateFlow = MutableStateFlow<Resource<CurrentWeatherState>?>(null)
+    private val _weatherStateFlow = MutableStateFlow<Resource<CurrentWeatherState>?>(Resource.Loading())
     val weatherStateFlow = _weatherStateFlow.asStateFlow()
 
-    private val _forecastStateFlow = MutableStateFlow<Resource<ForecastState>?>(null)
-    val forecastStateFlow = _forecastStateFlow.asStateFlow()
+    private val _weatherByCityStateFlow = MutableStateFlow<Resource<WeatherByCityState>?>(Resource.Loading())
+    val weatherByCityStateFlow = _weatherByCityStateFlow.asStateFlow()
     //end of section
 
 
     //Public API's Section
     fun getCurrentLocation() = viewModelScope.launch {
         locationRepository.getCurrentLocation().let { location ->
-            getCurrentWeather(location.latitude, location.longitude)
-        }
-
-        locationRepository.getCurrentLocation().let { location ->
             getCurrentForecast(location.latitude, location.longitude)
         }
+    }
+
+    fun getCityWeather(city: String) = viewModelScope.launch {
+        getCurrentWeatherByCity(city)
     }
     //end of section
 
 
     //Private Functions Section
-    private suspend fun getLocation() = locationRepository.getCurrentLocation()
-
-    private fun getCurrentWeather(lat: Double, lon: Double) = viewModelScope.launch {
-        getWeather(lat, lon).collect { weather ->
-            _weatherStateFlow.value = weather
-        }
-    }
-
-    private suspend fun getWeather(lat: Double, lon: Double) =
-        weatherRepository.getWeather(lat, lon).map { weather ->
-            weatherMapper.mapWeatherToWeatherState(weather)
-        }
-
     private fun getCurrentForecast(lat: Double, lon: Double) = viewModelScope.launch {
         getForecast(lat, lon).collect { forecast ->
-            _forecastStateFlow.value = forecast
+            _weatherStateFlow.value = forecast
         }
     }
 
     private suspend fun getForecast(lat: Double, lon: Double) =
         weatherRepository.getForecast(lat, lon).map { forecast ->
-            weatherMapper.mapForecastToForecastState(forecast)
+            weatherMapper.mapWeatherToWeatherState(forecast)
+        }
+
+    private fun getCurrentWeatherByCity(city: String) = viewModelScope.launch {
+        getWeatherByCity(city).collect { weather ->
+            _weatherByCityStateFlow.value = weather
+        }
+    }
+
+    private suspend fun getWeatherByCity(city: String) =
+        weatherRepository.getWeatherByCity(city).map { weather ->
+            cityWeatherMapper.mapCityWeatherToWeatherState(weather)
         }
     //end of section
 }

b7070252c73f53670c8d3934e78bacdcf957b62e
mandisikuse <mandisikuse@gmail.com>
2024-06-16 23:53:25 +0200

    Updating weather mapper

--- app/src/main/java/com/dvt/weatherapp/domain/mapper/WeatherMapper.kt
+++ app/src/main/java/com/dvt/weatherapp/domain/mapper/WeatherMapper.kt
@@ -1,44 +1,30 @@
 package com.dvt.weatherapp.domain.mapper
 
-import com.dvt.weatherapp.data.model.CurrentWeather
-import com.dvt.weatherapp.data.model.Forecast
-import com.dvt.weatherapp.data.model.WeatherData
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.toUpperCase
+import com.dvt.weatherapp.data.remote.model.Forecast
+import com.dvt.weatherapp.data.remote.model.WeatherData
 import com.dvt.weatherapp.ui.statemodel.CurrentWeatherState
 import com.dvt.weatherapp.ui.statemodel.Description
-import com.dvt.weatherapp.ui.statemodel.ForecastState
 import com.dvt.weatherapp.ui.statemodel.WeeklyForecast
 import com.dvt.weatherapp.util.Resource
 import com.dvt.weatherapp.util.getDayFromDate
 
 class WeatherMapper {
-    fun mapWeatherToWeatherState(response: Resource<CurrentWeather>) =
-        when (response) {
-            is Resource.Success -> {
-                response.data?.let { weatherData ->
-                    Resource.Success(
-                        CurrentWeatherState(
-                            current = weatherData.main?.temp?.toInt(),
-                            min = weatherData.main?.temp_min?.toInt(),
-                            max = weatherData.main?.temp_max?.toInt(),
-                            description = Description.valueOf(
-                                weatherData.weather?.main?.uppercase() ?: "CLEAR"
-                            )
-                        )
-                    )
-                } ?: Resource.Error(response.message)
-            }
-
-            is Resource.Error -> Resource.Error(response.message)
-
-            is Resource.Loading -> Resource.Loading()
-        }
-
-    fun mapForecastToForecastState(response: Resource<Forecast>) =
+    fun mapWeatherToWeatherState(response: Resource<Forecast>) =
         when (response) {
             is Resource.Success -> {
                 response.data?.list.let { forecastList ->
                     Resource.Success(
-                        ForecastState(
+                        CurrentWeatherState(
+                            current = forecastList?.first()?.main?.temp?.toInt() ?: 0,
+                            min = forecastList?.first()?.main?.temp_min?.toInt() ?: 0,
+                            max = forecastList?.first()?.main?.temp_max?.toInt() ?: 0,
+                            description = Description.valueOf(
+                                forecastList?.first()?.weather?.first()?.main?.toUpperCase(
+                                    Locale.current
+                                ) ?: "CLEAR"
+                            ),
                             forecast = forecastData(forecastList)
                         )
                     )
@@ -51,16 +37,27 @@ class WeatherMapper {
         }
 
     private fun forecastData(forecast: List<WeatherData>?): List<WeeklyForecast> {
+        val dayOfWeek = mutableSetOf<String>()
         val weatherList = mutableListOf<WeatherData>()
 
         forecast?.forEach { weatherData ->
-            weatherList.add(weatherData)
+            weatherData.dt_txt?.substringBefore(" ")?.let { day ->
+                if (day !in dayOfWeek) {
+                    dayOfWeek.add(day)
+                    weatherList.add(weatherData)
+                }
+            }
         }
+
+        if (weatherList.isNotEmpty()) {
+            weatherList.removeFirst()
+        }
+
         return weatherList.map {
             val date = it.dt_txt?.substringBefore(" ").toString()
             WeeklyForecast(
                 day = getDayFromDate(date),
-                temp = it.main?.temp?.toInt(),
+                temp = it.main?.temp?.toInt() ?: 0,
                 description = Description.valueOf(
                     it.weather?.first()?.main?.uppercase() ?: "CLEAR"
                 )

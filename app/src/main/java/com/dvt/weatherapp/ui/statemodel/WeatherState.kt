32ab758d59391af690d8dedbd44948af65d21f18
mandisikuse <mandisikuse@gmail.com>
2024-06-16 23:56:12 +0200

    Updating WeatherState.kt

--- app/src/main/java/com/dvt/weatherapp/ui/statemodel/WeatherState.kt
+++ app/src/main/java/com/dvt/weatherapp/ui/statemodel/WeatherState.kt
@@ -1,20 +1,25 @@
 package com.dvt.weatherapp.ui.statemodel
 
 data class CurrentWeatherState(
-    val current: Int? = 0,
-    val min: Int? = 0,
-    val max: Int? = 0,
-    val description: Description? = Description.CLEAR,
-)
-
-data class ForecastState(
+    val current: Int = 0,
+    val min: Int = 0,
+    val max: Int = 0,
+    val description: Description = Description.CLEAR,
     val forecast: List<WeeklyForecast> = emptyList()
 )
 
+data class WeatherByCityState(
+    val current: Int = 0,
+    val min: Int = 0,
+    val max: Int = 0,
+    val description: Description = Description.CLEAR
+)
+
+
 data class WeeklyForecast(
-    val day: String? = "",
-    val temp: Int? = 0,
-    val description: Description? = Description.CLEAR
+    val day: String = "",
+    val temp: Int = 0,
+    val description: Description = Description.CLEAR
 )
 
 enum class Description(main: String) {

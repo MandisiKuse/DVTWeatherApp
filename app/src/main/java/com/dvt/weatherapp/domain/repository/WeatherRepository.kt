fb258ca45f2d7cc73423e7d42c4f8e4c76b2d5e5
mandisikuse <mandisikuse@gmail.com>
2024-06-16 23:54:13 +0200

    Updating WeatherRepository

--- app/src/main/java/com/dvt/weatherapp/domain/repository/WeatherRepository.kt
+++ app/src/main/java/com/dvt/weatherapp/domain/repository/WeatherRepository.kt
@@ -1,12 +1,13 @@
 package com.dvt.weatherapp.domain.repository
 
-import com.dvt.weatherapp.data.model.CurrentWeather
-import com.dvt.weatherapp.data.model.Forecast
+import com.dvt.weatherapp.data.remote.model.CurrentWeather
+import com.dvt.weatherapp.data.remote.model.Forecast
 import com.dvt.weatherapp.util.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface WeatherRepository {
-    suspend fun getWeather(lat: Double, lon: Double): Flow<Resource<CurrentWeather>>
+
+    suspend fun getWeatherByCity(city: String): Flow<Resource<CurrentWeather>>
 
     suspend fun getForecast(lat: Double, lon: Double): Flow<Resource<Forecast>>
 }
